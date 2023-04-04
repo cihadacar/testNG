e627fc45fee817c95ff2269e7182deff263314c7
cihadacar <cihadacar1@hotmail.com>
2023-04-04 21:37:36 +0300

    first commit

---
+++ .idea/sonarlint/securityhotspotstore/index.pb
@@ -0,0 +1,7 @@
+
+Z
+*src/main/java/actions/HomePageActions.java,6\1\616be31d341d116a3da350b5384f5efe91282f1e
+T
+$src/test/java/definitions/Hooks.java,a\2\a264b8fab89e7879eedbb63a3b1386c0cc7082a2
+[
++src/main/java/actions/LoginPageActions.java,1\c\1c268cc83a3b7cd9b1d9673f731b315c505a603b
---
+++ src/test/java/runner/CucumberRunnerTests.java
@@ -0,0 +1,11 @@
+package runner;
+
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+
+@CucumberOptions(tags = "", features = "src/test/resources/features/LoginPage.feature",
+        glue = "definitions", plugin = {})
+
+public class CucumberRunnerTests extends AbstractTestNGCucumberTests {
+    
+}
